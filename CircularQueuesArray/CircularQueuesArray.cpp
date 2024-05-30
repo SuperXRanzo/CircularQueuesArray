474595b7c201dbcfa794166fbf5de377463801b7
SuperXRanzo <okanskyavenger@gmail.com>
2024-05-30 17:46:43 +0700

    cek apakah antrian kosong

--- CircularQueuesArray/CircularQueuesArray.cpp
+++ CircularQueuesArray/CircularQueuesArray.cpp
@@ -20,3 +20,9 @@ void insert() {
 	if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
 		cout << "\nQueue overflow\n";
 		return;
+	}
+
+	if (FRONT == -1) {
+		FRONT = 0;
+		REAR = 0;
+	}
